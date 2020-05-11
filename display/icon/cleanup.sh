e8016a14adee1edf77fa7c721d50aef08e550683
Raoul Rubien <rubienr@sbox.tugraz.at>
2020-05-12 00:01:46 +0200

    added scripts for cleanup icon folder

---
+++ display/icon/cleanup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+
+function cleanup()
+{
+  source cleanup.cfg
+  for f in "${FOLDERS_TO_CLEAR[@]}" ; do
+    rm -d $f
+    mkdir -p $f
+  done
+}
+
+
+function main()
+{
+  pushd $SCRIPT_DIR
+  cleanup
+  popd
+}
+
+main
