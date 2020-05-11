eca0ce6579e55cb0ae0501c9fda003bd52cb06d0
Raoul Rubien <rubienr@sbox.tugraz.at>
2020-05-11 21:10:17 +0200

    added scripts to convert boot screen

---
+++ resources/blender/export.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+
+function copy_ressources()
+{
+  source $SCRIPT_DIR/export.cfg
+  for filepath in "${BMP_FILES_TO_CONVERT[@]}" ; do
+    f_basename=`basename $filepath`
+    f_converted="./generated/$f_basename"
+
+    echo "copy file $f_basename "
+    echo "  -> ./generated/$f_basename"
+    cp -f "$filepath" "./"
+  done
+}
+
+
+function convert_all_ressources()
+{
+  # convert groups to 8 bit
+  for filepath in `find . -maxdepth 1 -name "*.bmp"` ; do
+    f_basename=`basename $filepath`
+    
+    echo "converting $f_basename"
+    convert $filepath -type truecolor $filepath
+  done
+}
+
+
+function main()
+{
+  pushd "$SCRIPT_DIR"
+  mkdir -p "generated"
+  pushd "$SCRIPT_DIR/generated"
+  
+  copy_ressources
+  convert_all_ressources
+  
+  popd
+  popd
+}
+
+main
