650d1e8b728e9197d98f6bfc78fc13e4088098f5
Raoul Rubien <rubienr@sbox.tugraz.at>
2020-05-13 21:42:45 +0200

    do more cleanup

---
+++ display/cleanup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+
+function cleanup()
+{
+  source cleanup.cfg
+  for f in "${FOLDERS_TO_CLEAR[@]}" ; do
+    echo "remove $f"
+    rm -drf $f
+  done
+}
+
+
+function main()
+{
+  echo -e "\nCleanup ..."
+  pushd $SCRIPT_DIR >/dev/null
+  cleanup
+  popd > /dev/null
+}
+
+main
